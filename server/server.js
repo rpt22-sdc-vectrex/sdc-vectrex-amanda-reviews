e0ac98c28e6b8d41f7253d54c2e2a1cdc1fd2ed5
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-08-08 16:30:29 -0700

    Webpack setup

--- server/server.js
+++ server/server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8888;
 
-app.get('/', (req, res) => {
-  res.send('Hello from express')
-})
+app.use(express.static('./public'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
