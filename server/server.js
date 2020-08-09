0e1eed89c886158652fef49fe0a90dd669831d34
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-08-09 13:41:31 -0700

    Add 404 for product not found (with new test)

--- server/server.js
+++ server/server.js
@@ -1,26 +1,25 @@
 const express = require('express');
 const app = express();
 const db = require('../database');
-const port = 8888;
 
 app.use(express.static('./public'));
-
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`)
-// });
 
 // endpoint for reviews data to return rating for different modules
 app.get('/reviews/:productId', function (req, res) {
   var id = req.params.productId;
 
-  var sql = `SELECT AVG(rating) as rating FROM reviews WHERE product_id = ${id}`;
+  var sql = `SELECT product_id, AVG(rating) as rating FROM reviews WHERE product_id = ${id}`;
 
   db.query(sql, (err, result) => {
     if (err) {
       console.log(err);
     } else {
-      rating = Math.round(result[0].rating * 2) / 2;
-      res.send({ productId: id, rating: rating });
+      if (!result[0].product_id) {
+        res.status(404).send('no record in database for this product');
+      } else {
+        rating = Math.round(result[0].rating * 2) / 2;
+        res.send({ productId: id, rating: rating });
+      }
     }
   })
 });
--- server/server.test.js
+++ server/server.test.js
@@ -1,25 +1,42 @@
-jest.mock('../database', () => {
-  return {
-    query: jest.fn((sql, cb) => {
-      console.log('Mock query was called with', sql);
-      cb(null, [{ rating: 3.65 }]);
-    })
-  };
-});
-
 const db = require('../database');
 const app = require('./server');
 const supertest = require('supertest');
 
-describe("Test the root path", () => {
-  test("It should response the GET method", () => {
+jest.mock('../database', () => {
+  return {
+    query: jest.fn((sql, cb) => {
+      // default case in my db, if product exist return rating
+      cb(null, [{ product_id: 1234, rating: 3.65 }]);
+    })
+  };
+});
+
+describe("It should return the correct response for a GET request", () => {
+  test("It should response the GET method with the corrected rating if product exist in db", () => {
     return supertest(app)
       .get("/reviews/1234")
       .then(response => {
         expect(response.statusCode).toBe(200);
-        expect(response.body).toEqual({"productId":"1234","rating":3.5});
+        expect(response.body).toEqual({ "productId": "1234", "rating": 3.5 });
         expect(db.query).toHaveBeenCalledWith(
-          "SELECT AVG(rating) as rating FROM reviews WHERE product_id = 1234", 
+          "SELECT product_id, AVG(rating) as rating FROM reviews WHERE product_id = 1234",
+          expect.any(Function));
+      });
+  });
+
+  test("It should response the GET method with a 404 if product is not in the db", () => {
+    // For this test mock database returns an array of single row with null values
+    db.query.mockImplementationOnce((sql, cb) => {
+      cb(null, [{ product_id: null, rating: null }]);
+    });
+
+    return supertest(app)
+      .get("/reviews/123")
+      .then(response => {
+        expect(response.statusCode).toBe(404);
+        expect(response.text).toEqual('no record in database for this product');
+        expect(db.query).toHaveBeenLastCalledWith(
+          "SELECT product_id, AVG(rating) as rating FROM reviews WHERE product_id = 123",
           expect.any(Function));
       });
   });
