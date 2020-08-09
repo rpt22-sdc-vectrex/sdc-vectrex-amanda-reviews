9ba056aa00b424f33d7ec41ec36cb0bcff18b374
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-08-09 14:32:02 -0700

    Add airbnb style guide

--- database/seed.js
+++ database/seed.js
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS reviews;
 CREATE TABLE reviews (id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,username VARCHAR(50) NOT NULL,\`text\` TEXT NOT NULL,rating INTEGER NOT NULL,\`date\` DATE NOT NULL,product_id INTEGER NOT NULL,user_profile_url VARCHAR(250) NOT NULL,store_id INTEGER NOT NULL);
 `;
 
-connection.query(queryString, function (err, result) {
+connection.query(queryString, function (err) {
   if (err) {
     console.log(err);
   } else {
@@ -120,7 +120,9 @@ connection.query(queryString, function (err, result) {
       if (err) {
         console.log(err);
       } else {
-        console.log(result);
+        for (var i = 0; i < result.length; i++) {
+          console.log(`A review with id ${result[i].insertId} was inserted.`);
+        }
       }
       connection.end();
     });
