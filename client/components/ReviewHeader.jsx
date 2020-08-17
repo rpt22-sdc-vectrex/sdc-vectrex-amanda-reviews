e497da50fb9bfa0cd9ee3ce4243ff85674136126
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-08-17 00:40:36 -0700

    Add moment js and fix time format

--- client/components/ReviewHeader.jsx
+++ client/components/ReviewHeader.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import moment from 'moment';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class ReviewHeader extends React.Component {
@@ -9,7 +10,7 @@ class ReviewHeader extends React.Component {
       <div>
         <img alt="profile" src={props.info.userPicture} />
         <a href={props.info.user_profile_url}>{props.info.username}</a>
-        <span>{props.info.date}</span>
+        <span>{moment(props.info.date).format('LL')}</span>
       </div>
     );
   }
