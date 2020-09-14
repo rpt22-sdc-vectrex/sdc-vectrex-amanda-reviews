8acf99c49e60df6256db68321bff94799ae6596c
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-09-13 23:38:18 -0700

    Add real data and small fixes

--- client/components/ReviewsWidget/ReviewsWidget.styles.jsx
+++ client/components/ReviewsWidget/ReviewsWidget.styles.jsx
@@ -4,6 +4,7 @@ const ReviewsWidgetContainer = styled.div`
   font: ${(props) => props.theme.fontShorthand.baseMain};
   letter-spacing: 0.4px;
   padding-left: 30px;
+  margin-top: 30px;
 `;
 
 export default ReviewsWidgetContainer;
